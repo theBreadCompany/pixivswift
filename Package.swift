4417edb38bc40e0bd28199df795d73971dd583f5
Fabio Mauersberger <thebreadcompany@icloud.com>
2021-07-01 20:52:08 +0200

    declutter

--- Package.swift
+++ Package.swift
@@ -16,8 +16,7 @@ let package = Package(
     targets: [
         .target(
             name: "pixivswift",
-            dependencies: ["SwiftyJSON", "Erik"],
-            exclude: ["Info.plist"]
+            dependencies: ["SwiftyJSON", "Erik"]
         )
     ]
 )
