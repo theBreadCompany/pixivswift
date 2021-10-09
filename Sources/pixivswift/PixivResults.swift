72e20161946da3f126998ceb948ce4d367e13bdf
Fabio Mauersberger <thebreadcompany@icloud.com>
2021-10-10 01:23:33 +0200

    fix generation of invalid original URLs

--- Sources/pixivswift/PixivResults.swift
+++ Sources/pixivswift/PixivResults.swift
@@ -3,7 +3,7 @@
 //  pixivswift
 //
 //  Created by Fabio Mauersberger on 03.10.21.
-//
+//  TODO: fix the imageURLs property to be usable; 
 
 import Foundation
 
@@ -50,7 +50,8 @@ public struct PixivIllustration: Codable {
             pages.append(IllustrationImageURLs(
                 squareMedium: URL(string: imageUrls.squareMedium.absoluteString.replacingOccurrences(of: "_p0", with: "_p\(n)"))!,
                 medium: URL(string: imageUrls.medium.absoluteString.replacingOccurrences(of: "_p0", with: "_p\(n)"))!,
-                large: URL(string: imageUrls.large.absoluteString.replacingOccurrences(of: "_p0", with: "_p\(n)"))!))
+                large: URL(string: imageUrls.large.absoluteString.replacingOccurrences(of: "_p0", with: "_p\(n)"))!,
+                original: metaSinglePage["original_image_url"] ?? URL(string: imageUrls.large.absoluteString.replacingOccurrences(of: "c/600x1200_90_webp/img-master", with: "img-original").replacingOccurrences(of: "_master1200", with: ""))!))
         }
         return pages
     }
@@ -69,7 +70,7 @@ public struct PixivIllustration: Codable {
     public var totalBookmarks: Int
     public var metaSinglePage: [String:URL]
     public var restrict: Int
-    public var imageUrls: IllustrationImageURLs
+    private var imageUrls: IllustrationImageURLs // this is genuinly unsatisfying, but the image_urls property in the json doesnt contain a "original" URL, using the meta_single_page one for that instead (which is only used for pageCount>1)
     public var caption: String
 }
 
@@ -108,12 +109,27 @@ public struct IllustrationTag: Codable {
 
 public struct IllustrationImageURLs: Codable {
     
+    enum CodingKeys: String, CodingKey { case squareMedium, medium, large, original }
+    
+    public init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        squareMedium = try values.decode(URL.self, forKey: .squareMedium)
+        medium = try values.decode(URL.self, forKey: .medium)
+        large = try values.decode(URL.self, forKey: .large)
+        original = try values.decodeIfPresent(URL.self, forKey: .original) ?? URL(string: large.absoluteString.replacingOccurrences(of: "c/600x1200_90_webp/img-master", with: "img-original").replacingOccurrences(of: "_master1200", with: ""))!
+    }
+    
+    init(squareMedium: URL, medium: URL, large: URL, original: URL) {
+        self.squareMedium = squareMedium
+        self.medium = medium
+        self.large = large
+        self.original = original
+    }
+    
     public var squareMedium: URL
     public var medium: URL
     public var large: URL
-    public var original: URL {
-        URL(string: large.absoluteString.replacingOccurrences(of: "c/600x1200_90_webp/img-master", with: "img-original").replacingOccurrences(of: "_master1200", with: ""))!
-    }
+    public var original: URL
 }
 
 public struct UgoiraMetadata: Codable {
