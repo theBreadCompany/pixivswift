f59ed310cca3652748b1bf607057d0322e63be0f
Fabio Mauersberger <thebreadcompany@icloud.com>
2021-10-09 16:57:47 +0200

    use runloop to block execution until a has response arrived

--- Sources/pixivswiftWrapper/PixivDownloader.swift
+++ Sources/pixivswiftWrapper/PixivDownloader.swift
@@ -362,9 +362,8 @@ open class PixivDownloader {
                 }
             }
             request.resume()
-            while request.response == nil { // wait for the response
-                continue
-            }
+            while request.response == nil && RunLoop.current.run(mode: .default, before: .distantFuture){} // wait for the response
+            
             if [200, 301, 302].contains((request.response as! HTTPURLResponse).statusCode) { // if the response signals a success, continue with setting the succeeded url and wait for it to be written to the disk
                 succeededURL = directory.appendingPathComponent(request.currentRequest!.url!.lastPathComponent)
                 while !FileManager.default.fileExists(atPath: succeededURL!.path) { continue }
