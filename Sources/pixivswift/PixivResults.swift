6663452092277cbba8cfbd377226c372b373a86c
theBreadCompany <thebreadcompany@icloud.com>
2023-11-27 12:07:52 +0100

    fix illust ai type

--- Sources/pixivswift/PixivResults.swift
+++ Sources/pixivswift/PixivResults.swift
@@ -100,7 +100,7 @@ public struct PixivIllustration: Codable {
         }
     }
     public var caption: String
-    public var illustAiType: Bool
+    public var illustAiType: Int // need to reverse to get actual enums
     public var illustBookStyle: Bool
 }
 
